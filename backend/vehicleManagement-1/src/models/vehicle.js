cb16054cd1d6ac29eab06749fd873748217d83bb
muhammedfariis <7591faris@gmail.com>
2026-01-12 18:17:19 +0530

    "mongoose schema created"

--- backend/vehicleManagement-1/src/models/vehicle.js
+++ backend/vehicleManagement-1/src/models/vehicle.js
@@ -1,11 +1,62 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({
-   
+const schema = new mongoose.Schema(
+  {
+    vehiclesNo: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
 
+    vehicleType: {
+      type: String,
+      enum: ["truck", "minitruck", "van", "container"],
+      default: true,
+    },
 
+    brand: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-}, { timestamps: true });
+    year: {
+      type: Number,
+    },
+
+    status: {
+      type: String,
+      enum: ["Active", "Maintainance", "In-Transist", "Retired"],
+      default: "Active",
+    },
+
+    currentMilage: {
+      type: Number,
+      default: 0,
+    },
+
+    nextServiceMilage: {
+      type: Number,
+      required: true,
+    },
+
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Auth",
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    insuranceExpiry: Date,
+    polutionExpiry: Date,
+  },
+  { timestamps: true }
+);
 
 const vehicles = mongoose.model("vehicle_management", schema);
 
